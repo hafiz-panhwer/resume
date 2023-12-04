585298b2ed984cba451a3cfc0f0714c83a029830
hafiz-panhwer <150000371+hafiz-panhwer@users.noreply.github.com>
2023-12-04 19:20:04 +0500

    Addes index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+// print_r($_SESSION);
+if(!isset($_SESSION["userid"])){
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body>
+    <?php
+    include "header.php";
+    ?>
+<div class="jumbotron text-center">
+  <h1 class="display-3">Congratulation!</h1>
+</div>
+<?php
+include "footer.php";                 
+?>                 
+</body>
+</html>
